21cfdec3b07b63a4cd6fe03edd081706035a4676
Danbka-Taranbka <danila.klim01@gmail.com>
2024-01-09 01:11:13 +0300

    auth added +strategies+guards

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Module({
+  imports: [
+    UsersModule, 
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('secretKey'),
+    }),
+    inject: [ConfigService],
+  })],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
+
+  auth(user: User) {
+    const payload = {sub: user.id};
+
+    return { acces_token: this.jwtService.sign(payload) };
+  }
+
+  async validatePassword(username: string, pass: string) {
+    const user = await this.usersService.findByUsername(username);
+
+    if (user) {
+      const isMatched = await bcrypt.compare(pass, user.password);
+      const { password, ...result } = user;
+
+      return isMatched ? result : user;
+    }
+
+    return null;
+  }
+}
